33f99ee84925606db1c74bc4b28c46aa066fd133
justinpraveen1417 <praveenc@thetopclassentertainment.com>
2022-10-09 00:15:56 +0530

    changed name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          sachin devops enginer
+          sachin senior devops enginer 
         </p>
         <a
           className="App-link"
